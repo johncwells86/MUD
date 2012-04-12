fe729e5b8cfa3779b0a9d0eb0e0c90f5b0d57607
Your Name <john.wells@utah.edu>
2012-04-12 14:40:22 -0600

    Things.lua added

---
+++ things.lua
@@ -0,0 +1,9 @@
+-- Things.lua --
+
+--[[ 
+-- The idea behind this is to have an abstract concept that encompasses everything that is 
+-- contained in the game. Things like Containers, mobiles, items, and descendents of these things.
+-- At this point, it has been decided that Rooms are exempt from this criterion, as they are 
+-- somewhat of a special case.
+--]]
+
